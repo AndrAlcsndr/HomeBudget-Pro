c9c032dc70cf5956a96ac7f1a06affb0a80584d4
AndrAlcsndr <andralcsndr@gmail.com>
2026-02-24 19:38:15 -0300

    inclusao de teste db

--- api/HomeBudget.API/HomeBudget.API/Controllers/WeatherForecastController.cs
+++ api/HomeBudget.API/HomeBudget.API/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace HomeBudget.API.Controllers
 {
@@ -11,5 +12,20 @@ namespace HomeBudget.API.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         ];
 
-    }
+        [HttpGet("test-db")]
+        public async Task<IActionResult> TestDb()
+        {
+            try
+            {
+                using var connection = new SqlConnection("Server=(localdb)\\MSSQLLocalDB;Database=homeBudgedApp;Trusted_Connection=False;TrustServerCertificate=False;");
+                await connection.OpenAsync();
+                return Ok("Conectado com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro ao conectar: {ex.Message}");
+            }
+        }
+
+}
 }
