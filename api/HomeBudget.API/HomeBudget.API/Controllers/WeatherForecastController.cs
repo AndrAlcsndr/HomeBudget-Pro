e0795f2705db0c245cccebbf3e28ca607441187f
AndrAlcsndr <andralcsndr@gmail.com>
2026-02-24 20:39:08 -0300

    remoção de teste

--- api/HomeBudget.API/HomeBudget.API/Controllers/WeatherForecastController.cs
+++ api/HomeBudget.API/HomeBudget.API/Controllers/WeatherForecastController.cs
@@ -12,20 +12,7 @@ namespace HomeBudget.API.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         ];
 
-        [HttpGet("test-db")]
-        public async Task<IActionResult> TestDb()
-        {
-            try
-            {
-                using var connection = new SqlConnection("Server=(localdb)\\MSSQLLocalDB;Database=homeBudgedApp;Trusted_Connection=False;TrustServerCertificate=False;");
-                await connection.OpenAsync();
-                return Ok("Conectado com sucesso!");
-            }
-            catch (Exception ex)
-            {
-                return BadRequest($"Erro ao conectar: {ex.Message}");
-            }
-        }
+       
 
-}
+    }
 }
