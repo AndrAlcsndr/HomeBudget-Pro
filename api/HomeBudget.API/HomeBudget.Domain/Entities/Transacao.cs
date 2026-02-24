8644e8608bf0455817b1a5ad35631aab4204c381
AndrAlcsndr <andralcsndr@gmail.com>
2026-02-24 08:25:40 -0300

    Implem. de entidade de transação

---
+++ api/HomeBudget.API/HomeBudget.Domain/Entities/Transacao.cs
@@ -0,0 +1,15 @@
+﻿namespace HomeBudget.Domain.Entities
+{
+    public class Transacao
+    {
+        public int Id { get; set; }
+        public int IdCategoria { get; set;  }
+        public int IdPessoa { get; set; }
+
+        public StatusTransacao Status { get; set; }
+
+        public double Receitas { get; set; } = 0;
+        public double Despesas { get; set; } = 0;
+
+    }
+}
