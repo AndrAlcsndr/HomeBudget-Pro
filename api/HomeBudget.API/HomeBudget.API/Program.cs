3b6098bd71462f3cca475923bba082832fca66e3
AndrAlcsndr <andralcsndr@gmail.com>
2026-02-24 19:24:26 -0300

    implem. do uso do sql server para nossa aplicação

--- api/HomeBudget.API/HomeBudget.API/Program.cs
+++ api/HomeBudget.API/HomeBudget.API/Program.cs
@@ -1,4 +1,13 @@
+
+using HomeBudget.Infra;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
