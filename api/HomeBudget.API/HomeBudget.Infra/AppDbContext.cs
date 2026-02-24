199871d7cdcf708b957b3215ee99a2ad786beb0f
AndrAlcsndr <andralcsndr@gmail.com>
2026-02-24 19:18:44 -0300

    Implem. de classe definidora dbContext

---
+++ api/HomeBudget.API/HomeBudget.Infra/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using HomeBudget.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeBudget.Infra
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {}
+
+       
+    }
+}
