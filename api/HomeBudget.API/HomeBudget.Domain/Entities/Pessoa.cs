7c7a381eb72374d43fe9e81cb4446565674202e6
AndrAlcsndr <andralcsndr@gmail.com>
2026-02-24 08:11:45 -0300

    implem. de entidade pessoa

---
+++ api/HomeBudget.API/HomeBudget.Domain/Entities/Pessoa.cs
@@ -0,0 +1,15 @@
+﻿using HomeBudget.Domain.Enums;
+
+namespace HomeBudget.Domain.Entities
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public List<int> IdTransacoes { get; set;  }
+        public string Nome { get; set; }
+        public StatusPessoa Status { get; set; }
+
+        public DateTime DataCriacao { get; set; }
+        public DateTime DataModificacao { get; set; }
+    }
+}
