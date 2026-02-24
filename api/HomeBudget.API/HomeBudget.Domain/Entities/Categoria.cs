9105c7ff3bdd12d0339896776a4073e16f3cddee
AndrAlcsndr <andralcsndr@gmail.com>
2026-02-24 08:55:36 -0300

    correção de relacionamento

--- api/HomeBudget.API/HomeBudget.Domain/Entities/Categoria.cs
+++ api/HomeBudget.API/HomeBudget.Domain/Entities/Categoria.cs
@@ -3,15 +3,15 @@
     public class Categoria
     {
         public int Id { get; set; }
-        public int IdPessoa { get; set;  }
-
-        public int IdTransacoes { get; set; }
+        public List<int> IdTransacoes { get; set; } = [];
 
         public required string Nome { get; set; }
         public string ? Descricao { get; set;  }
 
         public DateTime DataCriacao { get; set; }
         public DateTime DataModificacao { get; set; }
+
+        public List<Transacao> Transacoes { get; set; } = [];
 
     }
 }
