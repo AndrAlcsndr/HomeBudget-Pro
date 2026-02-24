4726831d4fc0675c4a97ba364d72ddffd9d7dfdb
AndrAlcsndr <andralcsndr@gmail.com>
2026-02-24 08:55:23 -0300

    mapping configuration da categoria

---
+++ api/HomeBudget.API/HomeBudget.Application/Mappings/CategoriaConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using HomeBudget.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HomeBudget.Infrastructure.Data.Configurations;
+
+public class CategoriaConfiguration : IEntityTypeConfiguration<Categoria>
+{
+    public void Configure(EntityTypeBuilder<Categoria> builder)
+    {
+        builder.ToTable("Categorias");
+
+        builder.HasKey(c => c.Id);
+
+        builder.Property(c => c.Nome)
+               .IsRequired()
+               .HasMaxLength(100);
+
+        builder.Property(c => c.Descricao)
+               .HasMaxLength(300);
+
+        builder.Property(c => c.DataCriacao)
+               .IsRequired();
+
+        builder.Property(c => c.DataModificacao)
+               .IsRequired();
+
+        builder.HasMany(c => c.Transacoes)
+               .WithOne(t => t.Categoria)
+               .HasForeignKey(t => t.IdCategoria)
+               .OnDelete(DeleteBehavior.Restrict);
+    }
+}
