8e909fcd6824a8504e650f592686af4988f4ffb6
AndrAlcsndr <andralcsndr@gmail.com>
2026-02-24 08:17:09 -0300

    implem. de entidade categoria

---
+++ api/HomeBudget.API/HomeBudget.Domain/Entities/Categoria.cs
@@ -0,0 +1,17 @@
+﻿namespace HomeBudget.Domain.Entities
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public int IdPessoa { get; set;  }
+
+        public int IdTransacoes { get; set; }
+
+        public required string Nome { get; set; }
+        public string ? Descricao { get; set;  }
+
+        public DateTime DataCriacao { get; set; }
+        public DateTime DataModificacao { get; set; }
+
+    }
+}
