5172fefd071f6790a2b5615d51529b79af4d4158
AndrAlcsndr <andralcsndr@gmail.com>
2026-02-25 12:41:59 -0300

    criação da camada de serviço da entidade Pessoa

--- api/HomeBudget.API/HomeBudget.Application/Services/PessoaAppService.cs
+++ api/HomeBudget.API/HomeBudget.Application/Services/PessoaAppService.cs
@@ -1,6 +1,16 @@
-﻿namespace HomeBudget.Application.Services
+﻿using HomeBudget.Application.DTOs.Pagination;
+using HomeBudget.Application.Interfaces;
+
+namespace HomeBudget.Application.Services
 {
     public class PessoaAppService
     {
+        private readonly IPessoaAppService _repository;
+
+        public PessoaAppService(IPessoaAppService repository)
+        {
+            _repository = repository;
+        }
+
     }
 }
