d47350c48827871266c784c66de035c2e1b67b42
AndrAlcsndr <andralcsndr@gmail.com>
2026-02-25 12:23:37 -0300

    Criação de controller para acesso a API de gerenciamento da pessoa

--- api/HomeBudget.API/HomeBudget.API/Controllers/PessoaController.cs
+++ api/HomeBudget.API/HomeBudget.API/Controllers/PessoaController.cs
@@ -1,7 +1,20 @@
-﻿namespace HomeBudget.API.Controllers
+﻿using HomeBudget.Application.DTOs.Pagination;
+using HomeBudget.Application.DTOs.PessoaDtos;
+using HomeBudget.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeBudget.API.Controllers
 {
-    public class PessoaController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PessoaController : ControllerBase
     {
 
+        private readonly IPessoaAppService _service;
+
+        public PessoaController(IPessoaAppService pessoaService)
+        {
+            _service = pessoaService;
+        }
     }
 }
