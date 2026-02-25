3534d3ba2d54870fba4494f90cdb0f2291b0b194
AndrAlcsndr <andralcsndr@gmail.com>
2026-02-25 16:28:13 -0300

    implem. inicial da camada de serviços e seus metodos basicos

--- api/HomeBudget.API/HomeBudget.Application/Services/PessoaAppService.cs
+++ api/HomeBudget.API/HomeBudget.Application/Services/PessoaAppService.cs
@@ -1,16 +1,55 @@
-﻿using HomeBudget.Application.DTOs.Pagination;
-using HomeBudget.Application.Interfaces;
+﻿using HomeBudget.Application.Common;
+using HomeBudget.Application.DTOs.Pagination;
+using HomeBudget.Application.DTOs.PessoaDtos;
+using HomeBudget.Domain.Interfaces;
 
 namespace HomeBudget.Application.Services
 {
     public class PessoaAppService
     {
-        private readonly IPessoaAppService _repository;
+        private readonly IPessoaRepository _repository;
 
-        public PessoaAppService(IPessoaAppService repository)
+        public PessoaAppService(IPessoaRepository repository)
         {
             _repository = repository;
         }
+
+        public async Task<PagedResult<PessoaDto>> GetPagedAsync(PagedRequest request)
+        {
+            var (items, total) = await _repository.GetPagedAsync(request);
+
+            return new PagedResult<PessoaDto>
+            {
+                Items = items,
+                Total = total,
+                Page = request.Page,
+                PageSize = request.PageSize
+            };
+        }
+
+        public Task<OperationResult<Guid>> CreateAsync(CreatePessoaDto dto)
+        {
+            return _repository.CreateAsync(dto);
+        }
+
+        public Task<OperationResult<PessoaDto>> GetByIdAsync(Guid id)
+        {
+            return _repository.GetByIdAsync(id);
+        }
+
+        Task<OperationResult<bool>> DeleteAsync(Guid id)
+        {
+            return _repository.DeleteAsync(id);
+        }
+        Task<OperationResult<bool>> UpdateAsync(UpdatePessoaDto dto)
+        {
+            return _repository.UpdateAsync(dto);
+        }
+
+
+
+
+
 
     }
 }
