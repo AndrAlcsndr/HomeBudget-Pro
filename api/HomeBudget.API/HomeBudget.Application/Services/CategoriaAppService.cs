07ddf7cc71a605fd5aeaa93eb3a2033a97327498
AndrAlcsndr <andralcsndr@gmail.com>
2026-02-26 15:38:19 -0300

    implem. inicial da criação de uma categoria

--- api/HomeBudget.API/HomeBudget.Application/Services/CategoriaAppService.cs
+++ api/HomeBudget.API/HomeBudget.Application/Services/CategoriaAppService.cs
@@ -30,7 +30,54 @@ namespace HomeBudget.Application.Services
             };
         }
 
-        public Task<OperationResult<Guid>> CreateAsync(CreateCategoriaDto dto)
+        public async Task<OperationResult<Guid>> CreateAsync(CreateCategoriaDto dto)
+        {
+            (bool flowControl, OperationResult<bool> value) = await ValidarDadosBasicosCategoria(dto);
+            if (!flowControl)
+            {
+                return OperationResult<Guid>.Fail(value.Message!);
+            }
+
+            var newCategoria = _mapper.Map<Categoria>(dto);
+
+            try
+            {
+                await _repository.AddAsync(newCategoria);
+                return OperationResult<Guid>.Ok(newCategoria.Id, "Categoria criada com sucesso.");
+
+            }
+            catch (Exception ex)
+            {
+                return OperationResult<Guid>.Fail($"Ocorreu um erro ao criar a categoria, {ex.Message}. Tente novamente mais tarde.");
+
+            }
+
+        }
+
+        
+
+
+        private async Task<(bool flowControl, OperationResult<bool> value)> ValidarDadosBasicosCategoria(CreateCategoriaDto createDto)
+        {
+            if (string.IsNullOrWhiteSpace(createDto.Nome))
+                return (flowControl: false, value: OperationResult<bool>.Fail("Nome de cadastro é obrigatório."));
+
+            if (await _repository.NomeExistente(createDto.Nome))
+                return (flowControl: false, value: OperationResult<bool>.Fail("Outro cadastro já possui este nome."));
+
+            if (createDto.Descricao?.Length > 400)
+                return (flowControl: false, value: OperationResult<bool>.Fail("Quantidade de caracteres além do permitido."));
+
+            if (string.IsNullOrWhiteSpace(createDto.Descricao))
+                return (flowControl: false, value: OperationResult<bool>.Fail("Campo descrição obrigatório."));
+
+            if (!Enum.IsDefined(typeof(TipoCategoria), createDto.Finalidade))
+                return (flowControl: false, value: OperationResult<bool>.Fail("Nenhuma finalidade para a categoria estabelecida."));
+
+            return (flowControl: true, value: OperationResult<bool>.Ok(true));
+        }
+
+        public async Task<OperationResult<CategoriaDto>> GetByIdAsync(Guid id)
         {
             return _repository.CreateAsync(dto);
         }
