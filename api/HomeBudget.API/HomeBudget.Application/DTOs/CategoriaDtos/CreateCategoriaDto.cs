6cc0b45feeb1ec0c1490218d087e3ce33baa5840
AndrAlcsndr <andralcsndr@gmail.com>
2026-02-26 15:33:34 -0300

    Inserção das propriedades do dto de criação da categoria

--- api/HomeBudget.API/HomeBudget.Application/DTOs/CategoriaDtos/CreateCategoriaDto.cs
+++ api/HomeBudget.API/HomeBudget.Application/DTOs/CategoriaDtos/CreateCategoriaDto.cs
@@ -1,6 +1,13 @@
-﻿namespace HomeBudget.Application.DTOs.CategoriaDtos
+﻿using HomeBudget.Domain.Enums;
+
+namespace HomeBudget.Application.DTOs.CategoriaDtos
 {
     public class CreateCategoriaDto
     {
+        public required string Nome { get; set; }
+        public required string Descricao { get; set; }
+
+        public required TipoCategoria Finalidade { get; set; }
+
     }
 }
