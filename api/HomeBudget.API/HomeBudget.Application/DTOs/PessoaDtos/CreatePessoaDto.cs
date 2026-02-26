15e02a672353a2c9076ca6e2ff693022b223cd81
AndrAlcsndr <andralcsndr@gmail.com>
2026-02-26 15:34:31 -0300

    implem. das propriedades da criação da Pessoa

--- api/HomeBudget.API/HomeBudget.Application/DTOs/PessoaDtos/CreatePessoaDto.cs
+++ api/HomeBudget.API/HomeBudget.Application/DTOs/PessoaDtos/CreatePessoaDto.cs
@@ -2,5 +2,9 @@
 {
     public class CreatePessoaDto
     {
+        public required string Nome { get; set; }
+        public required string Cpf { get; set; }
+        public int Idade { get; set; }
+
     }
 }
