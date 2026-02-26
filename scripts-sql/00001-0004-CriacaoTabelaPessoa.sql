fd0a63e0058b9dfe36c446e2eba23a046dd2cb96
AndrAlcsndr <andralcsndr@gmail.com>
2026-02-26 01:14:25 -0300

    implem. script sql para criação da table Pessoa

---
+++ scripts-sql/00001-0004-CriacaoTabelaPessoa.sql
@@ -0,0 +1,8 @@
+-- ===================================================================================
+-- Script:     0001-0004-CriacaoTabelaPessoa.sql
+-- Card:       Issue 004 - Criação da camada de serviços e repositories
+-- Objetivo:   Cria��o da tabela Pessoa com todos os campos exigidos
+-- Autor:      [André Alecsander Araújo Lopes]
+-- Data:       [26/02/2026]
+-- ===================================================================================
+
