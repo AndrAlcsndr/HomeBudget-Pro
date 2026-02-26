5899612388810ed872d9cc31bf02c542c7042ff3
AndrAlcsndr <andralcsndr@gmail.com>
2026-02-26 01:07:07 -0300

    implem. das interfaces

--- api/HomeBudget.API/HomeBudget.Application/Interfaces/ICategoriaAppService.cs
+++ api/HomeBudget.API/HomeBudget.Application/Interfaces/ICategoriaAppService.cs
@@ -1,6 +1,15 @@
-﻿namespace HomeBudget.Application.Interfaces
+﻿using HomeBudget.Application.Common;
+using HomeBudget.Application.DTOs.CategoriaDtos;
+using HomeBudget.Application.DTOs.Pagination;
+
+namespace HomeBudget.Application.Interfaces
 {
     public interface ICategoriaAppService
     {
+        Task<OperationResult<Guid>> CreateAsync(CreateCategoriaDto dto);
+        Task<OperationResult<CategoriaDto>> GetByIdAsync(Guid id);
+        Task<OperationResult<bool>> DeleteAsync(Guid id);
+        Task<OperationResult<bool>> UpdateAsync(UpdateCategoriaDto dto);
+        Task<PagedResult<CategoriaRequestDto>> GetPagedAsync(PagedRequest request);
     }
 }
--- api/HomeBudget.API/HomeBudget.Application/Interfaces/ITransacaoAppService.cs
+++ api/HomeBudget.API/HomeBudget.Application/Interfaces/ITransacaoAppService.cs
@@ -1,6 +1,15 @@
-﻿namespace HomeBudget.Application.Interfaces
+﻿using HomeBudget.Application.Common;
+using HomeBudget.Application.DTOs.Pagination;
+using HomeBudget.Application.DTOs.TransacaoDtos;
+
+namespace HomeBudget.Application.Interfaces
 {
     public interface ITransacaoAppService
     {
+        Task<PagedResult<TransacaoDto>> GetPagedAsync(PagedRequest request);
+        Task<OperationResult<Guid>> CreateAsync(CreateTransacaoDto dto);
+        Task<OperationResult<TransacaoDto>> GetByIdAsync(Guid id);
+        Task<OperationResult<bool>> DeleteAsync(Guid id);
+        Task<OperationResult<bool>> UpdateAsync(UpdateTransacaoDto dto);
     }
 }
