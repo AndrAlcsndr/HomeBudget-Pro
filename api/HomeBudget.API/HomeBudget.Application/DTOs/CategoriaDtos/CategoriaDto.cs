55b7529f55bd58038848be78a8d9991a334007cb
AndrAlcsndr <andralcsndr@gmail.com>
2026-02-26 15:31:49 -0300

    inclusao de novos campos para o DTO da categoria

--- api/HomeBudget.API/HomeBudget.Application/DTOs/CategoriaDtos/CategoriaDto.cs
+++ api/HomeBudget.API/HomeBudget.Application/DTOs/CategoriaDtos/CategoriaDto.cs
@@ -1,6 +1,17 @@
-﻿namespace HomeBudget.Application.DTOs.CategoriaDtos
+﻿using HomeBudget.Domain.Enums;
+
+namespace HomeBudget.Application.DTOs.CategoriaDtos
 {
     public class CategoriaDto
     {
+        public Guid Id { get; set; }
+        public List<Guid> IdTransacoes { get; set; } = [];
+
+        public required string Nome { get; set; }
+        public string? Descricao { get; set; }
+
+        public TipoCategoria Finalidade { get; set; }
+
+
     }
 }
