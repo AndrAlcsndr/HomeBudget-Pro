dd5b9a727ecc39514dab42c6a8b9a16f4c24719e
AndrAlcsndr <andralcsndr@gmail.com>
2026-02-26 15:40:32 -0300

    exportação dos metodos utilizados para a interface

--- api/HomeBudget.API/HomeBudget.Domain/Interfaces/ICategoriaRepository.cs
+++ api/HomeBudget.API/HomeBudget.Domain/Interfaces/ICategoriaRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace HomeBudget.Domain.Interfaces
+﻿using HomeBudget.Domain.Entities;
+
+namespace HomeBudget.Domain.Interfaces
 {
     public interface ICategoriaRepository
     {
+        Task AddAsync(Categoria categoria);
+        Task UpdateAsync(Categoria categoria);
+        Task DeleteAsync(Categoria categoria);
+        Task<Categoria?> GetByIdAsync(Guid id);
+        Task<bool> NomeExistente(string nome);
     }
 }
