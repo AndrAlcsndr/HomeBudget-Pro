7c65bbaf853231d5754d604dc274efb7c1e9ee08
AndrAlcsndr <andralcsndr@gmail.com>
2026-02-26 15:28:14 -0300

    arquivo de config do autoMapper da entidade Pessoa

---
+++ api/HomeBudget.API/HomeBudget.Application/AutoMapper/PessoaProfile/PessoaMapper.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using HomeBudget.Application.DTOs.CategoriaDtos;
+using HomeBudget.Application.DTOs.PessoaDtos;
+using HomeBudget.Domain.Entities;
+
+namespace HomeBudget.Application.AutoMapper.PessoaProfile
+{
+    public class PessoaProfile : Profile
+    {
+        public PessoaProfile() 
+        {
+            CreateMap<Pessoa, PessoaDto>();
+                
+        }
+    }
+}
