6f6f1f61e8c903335a6730a20ca700b1adca538e
AndrAlcsndr <andralcsndr@gmail.com>
2026-02-26 15:26:45 -0300

    setting do profile da entidades e suas DTOs

---
+++ api/HomeBudget.API/HomeBudget.Application/AutoMapper/CategoriaProfile/CategoriaMapper.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using HomeBudget.Application.DTOs.CategoriaDtos;
+using HomeBudget.Domain.Entities;
+
+namespace HomeBudget.Application.AutoMapper.CategoriaProfile
+{
+    public class CategoriaProfile : Profile
+    {
+        public CategoriaProfile() 
+        {
+            CreateMap<Categoria, CategoriaDto>()
+                .ForMember(dest => dest.IdTransacoes, opt => opt.MapFrom(src => src.Transacoes.Select(t => t.Id).ToList()))
+                .ReverseMap();
+
+            CreateMap<Categoria, UpdateCategoriaDto>()
+              .ReverseMap();
+
+            CreateMap<CreateCategoriaDto, UpdateCategoriaDto>()
+              .ReverseMap();
+        }
+
+    }
+}
