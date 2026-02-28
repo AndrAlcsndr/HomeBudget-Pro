57c5e60ae7a3525d023e7816a98d35c1f4fa3d32
AndrAlcsndr <andralcsndr@gmail.com>
2026-02-28 14:24:54 -0300

    implem. de layout e background do menu lateral

--- web/src/pages/LateralNavBar/LateralNavBar.tsx
+++ web/src/pages/LateralNavBar/LateralNavBar.tsx
@@ -1,32 +1,25 @@
 import { Nav } from "react-bootstrap";
-import { Link, useLocation } from "react-router-dom";
+import { Link, } from "react-router-dom";
 
 function LateralNavBar() {
   return (
     <div className="d-flex">
       <div
-        className="bg-dark d-flex flex-column p-3 text-white"
-        
-      >
+        className="d-flex flex-column p-3 bg-[#191919] w-[250px] h-full text-white position-fixed">
         <h4 className="mb-4">HomeBudgetApp</h4>
 
         <Nav className="flex-column">
-            <Nav.Item>
-                <Nav.Link as={Link} to="/">Dashboard</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link as={Link} to="/pessoas">Pessoas</Nav.Link>
-            </Nav.Item>
-             <Nav.Item>
-                <Nav.Link as={Link} to="/categorias">Categorias</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link as={Link} to="/">Transações</Nav.Link>
-            </Nav.Item>
-        
+          
         </Nav>
 
         <div className="mt-auto">
+          <Nav.Link
+            as={Link}
+            to="/logout"
+            className="text-white mt-4"
+          >
+            Login
+          </Nav.Link>
         </div>
       </div>
     </div>
