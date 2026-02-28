81aaa09e93cfbbbfdb1d9161895e18515460b0f5
AndrAlcsndr <andralcsndr@gmail.com>
2026-02-28 20:20:38 -0300

    implem. de exibição de horario

--- web/src/pages/DashBoard/components/DBSearchBar/SearchBar.tsx
+++ web/src/pages/DashBoard/components/DBSearchBar/SearchBar.tsx
@@ -1,9 +1,18 @@
+
 function SearchBar() {
   return (
     <>
       <div className="flex justify-between items-center  bg-gray-200 p-2 shadow-md w-full z-[9999]">
         {/* Exibição de data atual */}
-        
+        <h6 className="text-[#525256] font-semibold pt-2">
+          {" "}
+          {new Date().toLocaleDateString("pt-BR", {
+            weekday: "long",
+            year: "numeric",
+            month: "short",
+            day: "numeric",
+          })}
+        </h6>
 
        
       </div>
