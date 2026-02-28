d3658e17684cf392da28230e7d889f7117eeeced
AndrAlcsndr <andralcsndr@gmail.com>
2026-02-28 15:20:06 -0300

    Aplicação de rotas

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,27 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Base from "./pages/Base/Base";
+import DashBoard from "./pages/DashBoard/DashBoard";
 
 function App() {
-
   return (
-    <>
-      <div className="min-h-screen bg-gray-900 flex items-center justify-center">
-        <h1 className="text-4xl font-bold text-blue-400">
-          Página Inicial
-        </h1>
-      </div>
-    
-    </>
+    <BrowserRouter>
+      <Routes>
+        {/* Rota-pai base para que o menu lateral fique sempre visivel */}
+        <Route element={<Base />}>
+        {/* Rotas filhas  */}
+          <Route path="/" element={<DashBoard />} />
+          <Route path="/dashBoard" element={<DashBoard />} />
+          <Route path="/pessoas"  />
+          <Route path="/categorias"  />
+          <Route path="/transacoes"  />
+          <Route path="/login"  />
+          {/*  */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
