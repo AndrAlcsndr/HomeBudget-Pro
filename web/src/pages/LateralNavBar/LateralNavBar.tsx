ff97c0be248edf9296967745c448d53039f48a00
AndrAlcsndr <andralcsndr@gmail.com>
2026-02-28 14:35:57 -0300

    implem. de botões navbar e navLink para redirecionamento

--- web/src/pages/LateralNavBar/LateralNavBar.tsx
+++ web/src/pages/LateralNavBar/LateralNavBar.tsx
@@ -1,8 +1,8 @@
 import { Nav } from "react-bootstrap";
-import { Link, } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function LateralNavBar() {
-const menuItems = [
+  const menuItems = [
     { name: "Dashboard", path: "/" },
     { name: "Pessoas", path: "/pessoas" },
     { name: "Categorias", path: "/categorias" },
@@ -11,25 +11,25 @@ const menuItems = [
 
   return (
     <div className="d-flex">
-      <div
-        className="d-flex flex-column p-3 bg-[#191919] w-[250px] h-full text-white position-fixed">
+      <div className="d-flex flex-column p-3 bg-[#191919] w-[250px] h-full text-white position-fixed">
+        <div  className="d-flex flex-column p-3 bg-[#191919] w-[250px] h-full text-white position-fixed">
         <h4 className="mb-4">HomeBudgetApp</h4>
 
         <Nav className="flex-column">
           {menuItems.map((item) => (
-            
+            <Nav.Item key={item.path}>
+              <Nav.Link
+                as={Link}
+                to={item.path}
+                className={`text-white mb-2 ${
+                  location.pathname === item.path ? "bg-success rounded" : ""
+                }`}
+              >
+                {item.name}
+              </Nav.Link>
+            </Nav.Item>
           ))}
         </Nav>
-
-        <div className="mt-auto">
-          <Nav.Link
-            as={Link}
-            to="/logout"
-            className="text-white mt-4"
-          >
-            Login
-          </Nav.Link>
-        </div>
       </div>
     </div>
   );
