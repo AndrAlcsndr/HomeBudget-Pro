5af26319070b95853fae6e23904f83ffe1f6b9cd
AndrAlcsndr <andralcsndr@gmail.com>
2026-02-28 14:21:47 -0300

    implem. de botões

--- web/src/pages/LateralNavBar/LateralNavBar.tsx
+++ web/src/pages/LateralNavBar/LateralNavBar.tsx
@@ -1,8 +1,34 @@
+import { Nav } from "react-bootstrap";
+import { Link, useLocation } from "react-router-dom";
 
 function LateralNavBar() {
   return (
     <div className="d-flex">
-     
+      <div
+        className="bg-dark d-flex flex-column p-3 text-white"
+        
+      >
+        <h4 className="mb-4">HomeBudgetApp</h4>
+
+        <Nav className="flex-column">
+            <Nav.Item>
+                <Nav.Link as={Link} to="/">Dashboard</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link as={Link} to="/pessoas">Pessoas</Nav.Link>
+            </Nav.Item>
+             <Nav.Item>
+                <Nav.Link as={Link} to="/categorias">Categorias</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link as={Link} to="/">Transações</Nav.Link>
+            </Nav.Item>
+        
+        </Nav>
+
+        <div className="mt-auto">
+        </div>
+      </div>
     </div>
   );
 }
