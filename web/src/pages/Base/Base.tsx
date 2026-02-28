929ad651e6b843deb64c821ed8df97d1f2808650
AndrAlcsndr <andralcsndr@gmail.com>
2026-02-28 20:18:32 -0300

    reajuste da base do projeto para incorporar a barra de pesquisa

--- web/src/pages/Base/Base.tsx
+++ web/src/pages/Base/Base.tsx
@@ -1,18 +1,21 @@
 import LateralNavBar from "../../components/LateralNavBar/LateralNavBar";
 import { Outlet } from "react-router-dom";
-
+import SearchBar from "../DashBoard/components/DBSearchBar/SearchBar";
 
 function Base() {
   return (
     <>
-    <div className="d-flex" >
-       <LateralNavBar />
-       {/* Ajuste de espaçamento para exibição de conteudo */}
-       <div className="ml-[250px] p-[10px]"></div>
-       <Outlet />
-    </div>
+      <div className="d-flex">
+        <LateralNavBar />
+
+        {/* Ajuste de espaçamento para exibição de conteudo */}
+        <div className="ml-[250px]"></div>
+        <SearchBar />
+
+        <Outlet />
+      </div>
     </>
-  ) 
+  );
 }
 
 export default Base;
