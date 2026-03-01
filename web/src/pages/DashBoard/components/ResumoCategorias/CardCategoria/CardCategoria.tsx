6b7e2ee2bbc1aa5d9d476255ab8024b354857d13
AndrAlcsndr <andralcsndr@gmail.com>
2026-03-01 10:36:34 -0300

    implem. de dados do card

--- web/src/pages/DashBoard/components/ResumoCategorias/CardCategoria/CardCategoria.tsx
+++ web/src/pages/DashBoard/components/ResumoCategorias/CardCategoria/CardCategoria.tsx
@@ -12,6 +12,41 @@ function CardCategoria() {
           <h4 style={{ fontWeight: "600" }}>Categoria nome</h4>
         </div>
 
+         <div className="d-flex justify-content-between align-items-center">
+          {/* Informações */}
+          <div>
+            <div style={{ marginBottom: "1rem" }}>
+              <small style={{ color: "#6c757d" }}>Total das Receitas: </small>
+              <div style={{ fontWeight: "600", fontSize: "1.1rem" }}>
+                R$ {target.toLocaleString("Pt-BR")}
+              </div>
+            </div>
+
+            <div>
+              <small style={{ color: "#6c757d" }}>Total das Despesas: </small>
+              <div style={{ fontWeight: "600", fontSize: "1.1rem" }}>
+                R$ {target.toLocaleString("Pt-BR")}
+              </div>
+            </div>
+          </div>
+
+          {/* Saldo */}
+          <div style={{ width: 120, height: 120 }}>
+            <div
+              style={{
+                textAlign: "center",
+                marginTop: "0.5rem",
+                fontSize: "0.8rem",
+                color: "#6c757d",
+              }}
+            >
+              Saldo
+              <div style={{ fontWeight: "600", fontSize: "1.1rem" }}>
+                R$ {target.toLocaleString("Pt-BR")}
+              </div>
+            </div>
+          </div>
+        </div>
        
       </Card.Body>
     </Card>
