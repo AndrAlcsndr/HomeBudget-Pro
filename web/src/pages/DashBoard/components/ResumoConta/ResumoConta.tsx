d90edf483043d1109032fc28c6a687736d67d740
AndrAlcsndr <andralcsndr@gmail.com>
2026-03-01 10:39:16 -0300

    impl. de resumo de card da conta

--- web/src/pages/DashBoard/components/ResumoConta/ResumoConta.tsx
+++ web/src/pages/DashBoard/components/ResumoConta/ResumoConta.tsx
@@ -1,20 +1,10 @@
-import { Container, Row } from "react-bootstrap";
-import ResumoTransacoes from "../ResumoTransacoes/ResumoTransacoes";
+import { Container } from "react-bootstrap";
 
 function ResumoConta() {
   return (
     <>
       <Container className="bg-white p-4 space-y-10 rounded-lg shadow-md mb-6">
-        <Row>
-          <h4 className="text-2xl font-bold text-[#878787] mb-4">Transações</h4>
-          <ResumoTransacoes />
-        </Row>
-
-        <Row>
-          <h4 className="text-2xl font-bold text-[#878787] mb-4">
-            Gastos por categoria
-          </h4>
-        </Row>
+        
       </Container>
     </>
   );
