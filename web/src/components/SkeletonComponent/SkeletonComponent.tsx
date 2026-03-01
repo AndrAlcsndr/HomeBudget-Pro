48002f7d2343de0b8f5200fd51276c59f773c0b9
AndrAlcsndr <andralcsndr@gmail.com>
2026-03-01 14:07:32 -0300

    implem. das props padrões e renderização condicional

--- web/src/components/SkeletonComponent/SkeletonComponent.tsx
+++ web/src/components/SkeletonComponent/SkeletonComponent.tsx
@@ -1,15 +1,33 @@
+import { Box, Card, Skeleton, Stack } from '@mui/material'
+
 type SkeletonComponentProps = {
   show?: boolean
   as?: 'list' | 'file-list' | 'feed-list' | 'cards' | 'item'
   times?: number
 }
 
-function SkeletonComponent() {
-  return (
-    <div className="flex">
-      
-    </div>
-  );
+export default function SkeletonComponent({
+  show = false,
+  as = 'list',
+  times = 1
+}: SkeletonComponentProps) {
+  if (!show) return null
+
+ 
+
+  switch (as) {
+    case 'feed-list':
+      return <>{skeletonList()}</>
+
+    case 'cards':
+      return <>{skeletonCards()}</>
+
+    case 'item':
+      return skeletonItem()
+
+    case 'file-list':
+    case 'list':
+    default:
+      return <>{skeletonList()}</>
+  }
 }
-    
-export default SkeletonComponent;
