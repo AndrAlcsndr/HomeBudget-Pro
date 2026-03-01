b5f6d135ba7cde9e1a7cc9d9445334eb1200f6d2
AndrAlcsndr <andralcsndr@gmail.com>
2026-03-01 10:30:34 -0300

    Carrousel para apresentação das categorias

--- web/src/pages/DashBoard/components/ResumoCategorias/ResumoCategorias.tsx
+++ web/src/pages/DashBoard/components/ResumoCategorias/ResumoCategorias.tsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
+import Carousel from "react-bootstrap/Carousel";
+import CardCategoria from "./CardCategoria/CardCategoria";
 
 function ResumoCategorias() {
+  const [index, setIndex] = useState(0);
 
   return (
     <div style={{ width: "450px" }}>
+      <Carousel
+        activeIndex={index}
+        onSelect={(selectedIndex) => setIndex(selectedIndex)}
+        controls={false}
+        indicators={false}
+        interval={null}
+      >
+        <Carousel.Item>
+          <CardCategoria />
+        </Carousel.Item>
 
+      </Carousel>
+
+      
     </div>
   );
 }
