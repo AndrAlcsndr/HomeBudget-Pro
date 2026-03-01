2686ff556963a00d1c427d451f15677632bd14a0
AndrAlcsndr <andralcsndr@gmail.com>
2026-03-01 10:43:44 -0300

    card de resumo das transações

--- web/src/pages/DashBoard/components/ResumoTransacoes/ResumoTransacoes.tsx
+++ web/src/pages/DashBoard/components/ResumoTransacoes/ResumoTransacoes.tsx
@@ -1,22 +1,60 @@
-import { Button, Card } from "react-bootstrap";
-
+import { Card } from "react-bootstrap";
 
 function ResumoTransacoes() {
+  const target = 20000;
+
   return (
-    <>
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+    <Card
+      style={{
+        width: "22rem",
+        borderRadius: "16px",
+        padding: "1rem",
+      }}
+    >
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        {/* Header */}
+        <div className="d-flex justify-content-between align-items-center mb-3">
+          <h4 style={{ fontWeight: "600" }}>Gastos totais</h4>
+        </div>
+
+        <div className="d-flex justify-content-between align-items-center">
+          {/* Informações */}
+          <div>
+            <div style={{ marginBottom: "1rem" }}>
+              <small style={{ color: "#6c757d" }}>Total das Receitas: </small>
+              <div style={{ fontWeight: "600", fontSize: "1.1rem" }}>
+                  R$ {target.toLocaleString("Pt-BR")}
+              </div>
+            </div>
+
+            <div>
+              <small style={{ color: "#6c757d" }}>Total das Despesas: </small>
+              <div style={{ fontWeight: "600", fontSize: "1.1rem" }}>
+                R$ {target.toLocaleString("Pt-BR")}
+              </div>
+            </div>
+          </div>
+
+          {/* Gauge */}
+          <div style={{ width: 120, height: 120 }}>
+            <div
+              style={{
+                textAlign: "center",
+                marginTop: "0.5rem",
+                fontSize: "0.8rem",
+                color: "#6c757d",
+              }}
+            >
+              Saldo
+              <div style={{ fontWeight: "600", fontSize: "1.1rem" }}>
+                R$ {target.toLocaleString("Pt-BR")}
+              </div>
+            </div>
+          </div>
+        </div>
       </Card.Body>
     </Card>
-    </>
-  ) 
+  );
 }
 
 export default ResumoTransacoes;
