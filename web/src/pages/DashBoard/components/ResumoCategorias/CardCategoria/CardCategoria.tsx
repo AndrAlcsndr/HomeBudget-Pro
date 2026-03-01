7567cf318769541d993bf487854d922e69999c7c
AndrAlcsndr <andralcsndr@gmail.com>
2026-03-01 10:35:14 -0300

    criação de card de resumo para a categoria

---
+++ web/src/pages/DashBoard/components/ResumoCategorias/CardCategoria/CardCategoria.tsx
@@ -0,0 +1,14 @@
+import { Card } from "react-bootstrap";
+
+function CardCategoria() {
+ //Valor mockado para demonstração
+  const target = 20000;
+
+  return (
+    <Card className="w-[22rem] border-[16px] p-[1rem]">
+    
+    </Card>
+  );
+}
+
+export default CardCategoria;
