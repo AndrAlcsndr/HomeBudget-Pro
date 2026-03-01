17acb613acf214f6edc06f9fa1dc82cf70cb03e0
AndrAlcsndr <andralcsndr@gmail.com>
2026-03-01 13:56:26 -0300

    implem. de dto de operação de requisição

--- web/src/interfaces/Operation/OperationRequest.ts
+++ web/src/interfaces/Operation/OperationRequest.ts
@@ -1,13 +1,21 @@
-export class OperationResult<T> {
-  success: boolean
-  data: T | null
-  message: string | null
-  statusCode: number
+export class OperationRequest {
+  page: number
+  pageSize: number
+  search?: string
+  sortBy?: string
+  sortDir?: string
+  tipo?: string
+  dataInicial?: Date
+  dataFinal?: Date
 
-  constructor(init?: Partial<OperationResult<T>>) {
-    this.success = init?.success ?? false
-    this.data = init?.data ?? null
-    this.message = init?.message ?? null
-    this.statusCode = init?.statusCode ?? 0
+  constructor(init?: Partial<OperationRequest>) {
+    this.page = init?.page ?? 1
+    this.pageSize = init?.pageSize ?? 20
+    this.search = init?.search
+    this.sortBy = init?.sortBy
+    this.sortDir = init?.sortDir
+    this.tipo = init?.tipo
+    this.dataInicial = init?.dataInicial
+    this.dataFinal = init?.dataFinal
   }
 }
