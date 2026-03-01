584c4b96cca33dc67c0d54951f1b8f834f41c5a3
AndrAlcsndr <andralcsndr@gmail.com>
2026-03-01 13:57:04 -0300

    criação de camada de serviços no frontend

--- web/src/services/PessoaService/pessoaService.ts
+++ web/src/services/PessoaService/pessoaService.ts
@@ -1,4 +1,50 @@
+import axios from 'axios'
 
-export class apiPessoaService {
-  
+import type { PessoaDto } from '../../interfaces/PessoaDtos/PessoaDto'
+import type { CreatePessoaDto } from '../../interfaces/PessoaDtos/CreatePessoaDto'
+import type { FinancasPessoaFiltroDto } from '../../interfaces/PessoaDtos/FinancasPessoaFiltroDto'
+import type { OperationResult } from '../../interfaces/Operation/OperationResult'
+import type { UpdatePessoaDto } from '../../interfaces/PessoaDtos/UpdatePessoaDto'
+import type { OperationRequest } from '../../interfaces/Operation/OperationRequest'
+
+
+export class ApiPessoaService {
+  private baseUrl = '/pessoa'
+  private api = axios.create({
+  baseURL: import.meta.env.VITE_API_BACK_URL,
+})
+
+  // Create
+  async create(request: CreatePessoaDto): Promise<PessoaDto> {
+    const response = await this.api.post<PessoaDto>(this.baseUrl, request)
+    return response.data
+  }
+
+  // GetPaged
+  async getPaged(request: OperationRequest): Promise<OperationResult<FinancasPessoaFiltroDto>> {
+    const response = await this.api.get<OperationResult<FinancasPessoaFiltroDto>>(
+      `${this.baseUrl}/paged`,
+      { params: request }
+    )
+
+    return response.data
+  }
+
+  // GetById
+  async getById(id: string): Promise<PessoaDto> {
+    const response = await this.api.get<PessoaDto>(`${this.baseUrl}/${id}`)
+    return response.data
+  }
+
+  // Update
+  async update(id: string, request: UpdatePessoaDto): Promise<PessoaDto> {
+    const response = await this.api.put<PessoaDto>(`${this.baseUrl}/${id}`, request)
+    return response.data
+  }
+
+  // Delete
+  async delete(id: string): Promise<boolean> {
+    const response = await this.api.delete<boolean>(`${this.baseUrl}/${id}`)
+    return response.data
+  }
 }
