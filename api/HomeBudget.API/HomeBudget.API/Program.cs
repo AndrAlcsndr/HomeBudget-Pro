b6718fe1b874e5af4b4a8997410d91f7e6c999ce
AndrAlcsndr <andralcsndr@gmail.com>
2026-03-01 19:03:46 -0300

    remoção de migration automatizada do banco

--- api/HomeBudget.API/HomeBudget.API/Program.cs
+++ api/HomeBudget.API/HomeBudget.API/Program.cs
@@ -49,12 +49,6 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
-}
-
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
