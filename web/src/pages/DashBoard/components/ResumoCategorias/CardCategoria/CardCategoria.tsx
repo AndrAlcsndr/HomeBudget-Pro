7ebe11e68e1e0961183dc134f5d858e377cfeb4e
AndrAlcsndr <andralcsndr@gmail.com>
2026-03-01 10:35:47 -0300

    implem. de header

--- web/src/pages/DashBoard/components/ResumoCategorias/CardCategoria/CardCategoria.tsx
+++ web/src/pages/DashBoard/components/ResumoCategorias/CardCategoria/CardCategoria.tsx
@@ -1,12 +1,19 @@
 import { Card } from "react-bootstrap";
 
 function CardCategoria() {
- //Valor mockado para demonstração
+  //Valor mockado para demonstração
   const target = 20000;
 
   return (
     <Card className="w-[22rem] border-[16px] p-[1rem]">
-    
+      <Card.Body>
+        {/* Header */}
+        <div className="d-flex justify-content-between align-items-center mb-3">
+          <h4 style={{ fontWeight: "600" }}>Categoria nome</h4>
+        </div>
+
+       
+      </Card.Body>
     </Card>
   );
 }
