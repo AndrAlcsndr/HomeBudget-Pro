9ce545b15cf7b129f777a95998a952a90aacadb6
AndrAlcsndr <andralcsndr@gmail.com>
2026-03-01 15:08:12 -0300

    implem. do component skeleton no layout

--- web/src/pages/PessoaView/PessoaView.tsx
+++ web/src/pages/PessoaView/PessoaView.tsx
@@ -1,8 +1,21 @@
 function PessoaView() {
   return (
-    <div className="flex">
-      PessoaView
+    <>
+      <div className="mb-10 space-y-4">
+        <div className="flex items-center justify-between">
+          <span className="text-2xl font-bold ">Pessoas</span>
+          <Button variant="primary" onClick={() => setLoading(!loading)}>
+            +Novo
+          </Button>
+        </div>
+
+        {loading ? (
+          <SkeletonComponent show={true} as="list" times={3} />
+        ) : (
+          <DataListComponent columns={columns} rows={rows} />
+        )}
     </div>
+    </>
   );
 }
 
