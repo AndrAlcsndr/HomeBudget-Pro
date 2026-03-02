69785085c87af1b24f8337dfb81e694ca73e2eb1
AndrAlcsndr <andralcsndr@gmail.com>
2026-03-02 00:03:06 -0300

    inclusão de mais mapeamentos entre dtos

--- api/HomeBudget.API/HomeBudget.Application/AutoMapper/PessoaProfile/PessoaMapper.cs
+++ api/HomeBudget.API/HomeBudget.Application/AutoMapper/PessoaProfile/PessoaMapper.cs
@@ -1,5 +1,4 @@
 ﻿using AutoMapper;
-using HomeBudget.Application.DTOs.CategoriaDtos;
 using HomeBudget.Application.DTOs.PessoaDtos;
 using HomeBudget.Domain.Entities;
 
@@ -7,10 +6,25 @@ namespace HomeBudget.Application.AutoMapper.PessoaProfile
 {
     public class PessoaProfile : Profile
     {
-        public PessoaProfile() 
+        public PessoaProfile()
         {
-            CreateMap<Pessoa, PessoaDto>();
-                
+            // Entity -> DTO
+            CreateMap<Pessoa, PessoaDto>().ReverseMap();
+
+            // DTO -> Entity (Create)
+            CreateMap<CreatePessoaDto, Pessoa>()
+                .ForMember(dest => dest.DataCriacao, opt => opt.Ignore())
+                .ForMember(dest => dest.DataModificacao, opt => opt.Ignore())
+                .ForMember(dest => dest._transacoes, opt => opt.Ignore());
+
+            // Entity -> CreateDto 
+            CreateMap<Pessoa, CreatePessoaDto>();
+
+            // UpdateDto <- -> CreateDto
+            CreateMap<UpdatePessoaDto, CreatePessoaDto>().ReverseMap();
+
+            // UpdateDto  -> EntityDto
+            CreateMap<UpdatePessoaDto, Pessoa>().ReverseMap();
         }
     }
 }
