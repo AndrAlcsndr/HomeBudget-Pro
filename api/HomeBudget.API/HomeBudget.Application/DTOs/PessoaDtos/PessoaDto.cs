4962a3fcc0cec36fa98d5fae1faada93c3fa3c09
AndrAlcsndr <andralcsndr@gmail.com>
2026-03-02 00:03:44 -0300

    implem. dos campos dto a serem exibidos em pesquisas

--- api/HomeBudget.API/HomeBudget.Application/DTOs/PessoaDtos/PessoaDto.cs
+++ api/HomeBudget.API/HomeBudget.Application/DTOs/PessoaDtos/PessoaDto.cs
@@ -2,5 +2,10 @@
 {
     public class PessoaDto
     {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public int Idade { get; set; }
+        public DateTime DataCriacao { get; set; }
     }
 }
