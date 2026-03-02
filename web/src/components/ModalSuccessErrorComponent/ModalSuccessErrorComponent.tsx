9f2e3abfcbf276226d1deddaf2d235c1e8aea592
AndrAlcsndr <andralcsndr@gmail.com>
2026-03-02 00:17:48 -0300

    implem. de modal para exibir mensagens de erro ou sucesso vindos do back

---
+++ web/src/components/ModalSuccessErrorComponent/ModalSuccessErrorComponent.tsx
@@ -0,0 +1,12 @@
+
+function ModalSuccessError({ show, type, message, onClose }: Props) {
+  const isSuccess = type === "success";
+
+  return (
+    <Modal show={show} onHide={onClose} centered>
+      
+    </Modal>
+  );
+}
+
+export default ModalSuccessError;
