6ff8da5254c478cf9e583bcebaea084814b90b62
AndrAlcsndr <andralcsndr@gmail.com>
2026-03-02 15:56:18 -0300

    adição de campos aos dtos

--- api/HomeBudget.API/HomeBudget.Application/DTOs/TransacaoDtos/CreateTransacaoDto.cs
+++ api/HomeBudget.API/HomeBudget.Application/DTOs/TransacaoDtos/CreateTransacaoDto.cs
@@ -14,6 +14,5 @@ namespace HomeBudget.Application.DTOs.TransacaoDtos
         public string Descricao { get; set; } = string.Empty;
         public double Receitas { get; set; } = 0.00;
         public double Despesas { get; set; } = 0.00;
-        public double Saldo { get; set; } = 0.00;
     }
 }
--- api/HomeBudget.API/HomeBudget.Application/DTOs/TransacaoDtos/TransacaoDto.cs
+++ api/HomeBudget.API/HomeBudget.Application/DTOs/TransacaoDtos/TransacaoDto.cs
@@ -15,5 +15,13 @@ namespace HomeBudget.Application.DTOs.TransacaoDtos
         public double Receitas { get; set; }
         public double Despesas { get; set; }
 
+        public double SomaReceitas { get; set; }
+        public double SomaDespesas { get; set; }
+
+        public double Saldo { get; set; }
+
+        public DateTime DataCriacao { get; set; }
+        public DateTime DataModificacao { get; set; }
+
     }
 }
