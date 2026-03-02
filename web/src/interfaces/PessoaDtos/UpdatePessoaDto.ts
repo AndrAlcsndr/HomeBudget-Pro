7bacda0e53b7795e3b19037c55cbfb2b34fab013
AndrAlcsndr <andralcsndr@gmail.com>
2026-03-02 00:19:08 -0300

    implem. de campo para que nao se perca na atualização seguinte

--- web/src/interfaces/PessoaDtos/UpdatePessoaDto.ts
+++ web/src/interfaces/PessoaDtos/UpdatePessoaDto.ts
@@ -3,16 +3,19 @@ export class UpdatePessoaDto {
   nome: string = "";
   idade: number = 0;
   cpf: string = "";
+  dataCriacao: string = new Date().toISOString();
 
   constructor(
     id: string = '',
     nome: string = "",
     cpf: string = "",
     idade: number = 0,
+    dataCriacao: string = new Date().toISOString(),
   ) {
     this.id = id;
     this.nome = nome;
     this.idade = idade;
     this.cpf = cpf;
+    this.dataCriacao = dataCriacao
   }
 }
