f50c7bbd17f276fbde671e140dbf872f7691e185
AndrAlcsndr <andralcsndr@gmail.com>
2026-03-02 12:05:51 -0300

    implem. de dtos da transacao

--- api/HomeBudget.API/HomeBudget.Application/DTOs/TransacaoDtos/TransacaoDto.cs
+++ api/HomeBudget.API/HomeBudget.Application/DTOs/TransacaoDtos/TransacaoDto.cs
@@ -1,6 +1,19 @@
-﻿namespace HomeBudget.Application.DTOs.TransacaoDtos
+﻿using HomeBudget.Domain.Enums;
+
+namespace HomeBudget.Application.DTOs.TransacaoDtos
 {
     public class TransacaoDto
     {
+        public Guid Id { get; set; }
+        public string Categoria { get; set; }
+        public string Pessoa { get; set; }
+
+        public string Descricao { get; set; }
+
+        public TipoTransacao Tipo { get; set; }
+
+        public double Receitas { get; set; }
+        public double Despesas { get; set; }
+
     }
 }
--- api/HomeBudget.API/HomeBudget.Domain/Entities/Transacao.cs
+++ api/HomeBudget.API/HomeBudget.Domain/Entities/Transacao.cs
@@ -8,14 +8,16 @@ namespace HomeBudget.Domain.Entities
         public Guid? IdCategoria { get; set;  }
         public Guid? IdPessoa { get; set; }
 
-        //public StatusTransacao Status { get; set; }
-
-        public Tipo Tipo { get; set; }
+        public TipoTransacao Tipo { get; set; }
 
         public string Descricao { get; set; }  = string.Empty;
         public double Receitas { get; set; } = 0.00;
         public double Despesas { get; set; } = 0.00;
         public double Saldo { get; set; } = 0.00;
+
+        public DateTime DataCriacao { get; set;  }
+        public DateTime DataModificacao { get; set;  }
+
 
         public Pessoa? Pessoa { get; set; }
         public Categoria? Categoria { get; set; }
