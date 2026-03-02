23340af2ec6f1198e15b68f91bf69a8aa8aa3550
AndrAlcsndr <andralcsndr@gmail.com>
2026-03-02 00:23:34 -0300

    implem. de botão para abrir a modal de add pessoa

--- web/src/pages/PessoaView/PessoaView.tsx
+++ web/src/pages/PessoaView/PessoaView.tsx
@@ -1,26 +1,42 @@
 import { useEffect, useState } from "react";
 import { ApiPessoaService } from "../../services/PessoaService/pessoaService";
-import { FinancasPessoaFiltroDto } from "../../interfaces/PessoaDtos/FinancasPessoaFiltroDto";
 import { Button } from "react-bootstrap";
+
+import type { CreatePessoaDto } from "../../interfaces/PessoaDtos/CreatePessoaDto";
+import type { UpdatePessoaDto } from "../../interfaces/PessoaDtos/UpdatePessoaDto";
 
 import DataListComponent from "../../components/DataGridComponent/DataGridComponent";
 import SkeletonComponent from "../../components/SkeletonComponent/SkeletonComponent";
-
+import ModalCriarEditarPessoa from "../../components/ModalCriarEditarPessoalComponent/ModalCriarEditarPessoa";
+import ModalSuccessError from "../../components/ModalSuccessErrorComponent/ModalSuccessErrorComponent";
+import { empty_Guid } from "../../utils/guid";
+import { isValidCpf, maskCPF, onlyDigits } from "../../utils/cpfValidator";
+import type { PessoaDto } from "../../interfaces/PessoaDtos/PessoaDto";
+import { formatDate } from "../../utils/formatDate";
+import {
+  faEdit,
+  faTrash,
+  faUserSlash,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import ModalConfirm from "../../components/ModalConfirmarCancelarComponent/ModalConfirmarCancelarComponent";
+import ModalConfirmarCancelar from "../../components/ModalConfirmarCancelarComponent/ModalConfirmarCancelarComponent";
 
 function PessoaView() {
-  const [rows, setRows] = useState<FinancasPessoaFiltroDto[]>([]);
-  const [loading, setLoading] = useState(false);
   const api = new ApiPessoaService();
+  const [rows, setRows] = useState<PessoaDto[]>([]);
+  const [openModal, setOpenModal] = useState(false);
 
-  useEffect(() => {
-    const fetchData = async () => {
+
+  const fetchData = async () => {
+      setLoading(true);
       try {
         const result = await api.getPaged({
           page: 1,
           pageSize: 10,
         });
 
-        setRows(result.data ?? ([] as FinancasPessoaFiltroDto[]));
+        setRows([...(result.items ?? [])]);
       } catch (error) {
         console.error("Erro ao buscar pessoas:", error);
       } finally {
@@ -28,28 +44,16 @@ function PessoaView() {
       }
     };
 
+  useEffect(() => {
     fetchData();
   }, []);
-
-  const columns = [
-    { field: "nome", headerName: "Nome", width: 150 },
-    { field: "idade", headerName: "Idade", width: 110 },
-    { field: "cpf", headerName: "CPF", width: 150 },
-    {
-      field: "dataCriacaoModificacao",
-      headerName: "Data de Modificação",
-      width: 200,
-      valueFormatter: (params: any) =>
-        new Date(params.value).toLocaleString("pt-BR"),
-    },
-  ];
 
   return (
     <>
       <div className="mb-10 space-y-4">
         <div className="flex items-center justify-between">
           <span className="text-2xl font-bold ">Pessoas</span>
-          <Button variant="primary" onClick={() => setLoading(!loading)}>
+          <Button variant="primary" onClick={() => setOpenModal(true)}>
             +Novo
           </Button>
         </div>
