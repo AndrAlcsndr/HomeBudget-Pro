36ab512d4c63972d5cf32996943fa4a993d378cd
AndrAlcsndr <andralcsndr@gmail.com>
2026-03-02 12:06:37 -0300

    add de comentarios

--- api/HomeBudget.API/HomeBudget.Application/AutoMapper/CategoriaProfile/CategoriaMapper.cs
+++ api/HomeBudget.API/HomeBudget.Application/AutoMapper/CategoriaProfile/CategoriaMapper.cs
@@ -9,21 +9,26 @@ namespace HomeBudget.Application.AutoMapper.CategoriaProfile
     {
         public CategoriaProfile() 
         {
+            // Mapping Categoria -> CategoriaDto
             CreateMap<Categoria, CategoriaDto>()
             .ForMember(dest => dest.IdTransacoes,
                 opt => opt.MapFrom(src => src.Transacoes.Select(t => t.Id).ToList()))
             .ReverseMap()
             .ForMember(dest => dest.Transacoes, opt => opt.Ignore());
 
+            // Mapping CreateCategoriaDto <- -> Categoria
             CreateMap<CreateCategoriaDto, Categoria>().ReverseMap();
 
+            // Mapping CategoriaDto  -> Categoria
             CreateMap<CategoriaDto, Categoria>(); CreateMap<Categoria, CategoriaDto>()
                 .ForMember(dest => dest.Finalidade,
                 opt => opt.MapFrom(src => src.Finalidade.ToString()));
 
+            // Mapping Categoria <- -> UpdateCategoriaDto
             CreateMap<Categoria, UpdateCategoriaDto>()
               .ReverseMap();
 
+            // Mapping CreateCategoriaDto <- -> UpdateCategoriaDto
             CreateMap<CreateCategoriaDto, UpdateCategoriaDto>()
               .ReverseMap();
         }
