aaca3448cd036502b2bdeb2e0bd4c5b646f88424
AndrAlcsndr <andralcsndr@gmail.com>
2026-03-02 00:09:15 -0300

    inclusão de comentario

--- api/HomeBudget.API/HomeBudget.Domain/Entities/Pessoa.cs
+++ api/HomeBudget.API/HomeBudget.Domain/Entities/Pessoa.cs
@@ -12,6 +12,7 @@ namespace HomeBudget.Domain.Entities
         public DateTime DataCriacao { get; set; }
         public DateTime DataModificacao { get; set; }
 
+        // uso de listagem interna para operações posteriores com os itens da lista
         public readonly List<Transacao> _transacoes = [];
 
         public IReadOnlyCollection<Transacao> Transacoes => _transacoes;
