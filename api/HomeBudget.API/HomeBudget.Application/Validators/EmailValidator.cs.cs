919c56afde67bad3e30fcc1df0f29c84d865263b
AndrAlcsndr <andralcsndr@gmail.com>
2026-03-02 00:07:25 -0300

    validação de email

---
+++ api/HomeBudget.API/HomeBudget.Application/Validators/EmailValidator.cs.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace HomeBudget.Application.Validators
+{
+    public static class EmailValidator
+    {
+
+        public static bool ValidarEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+
+            string pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            return Regex.IsMatch(email, pattern, RegexOptions.IgnoreCase);
+        }
+
+    }
+}
