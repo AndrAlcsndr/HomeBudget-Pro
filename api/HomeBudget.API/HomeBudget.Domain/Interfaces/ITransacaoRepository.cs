e10589cc0f542815ced8c492f63fe58d31c1d1a6
AndrAlcsndr <andralcsndr@gmail.com>
2026-03-02 12:07:20 -0300

    implem. de metodos adicionais a camada de repositorios

--- api/HomeBudget.API/HomeBudget.Domain/Interfaces/ITransacaoRepository.cs
+++ api/HomeBudget.API/HomeBudget.Domain/Interfaces/ITransacaoRepository.cs
@@ -1,6 +1,17 @@
-﻿namespace HomeBudget.Domain.Interfaces
+﻿using HomeBudget.Domain.Entities;
+
+namespace HomeBudget.Domain.Interfaces
 {
-    public interface ITransacaoRepository
+    public interface ITransacaoRepository<T> where T: class
     {
+        Task<(IReadOnlyList<Transacao> Items, int Total)> GetPagedAsync(T request);
+
+        Task<Transacao?> GetByIdAsync(Guid id); 
+
+        Task AddAsync(Transacao transacao);
+
+        void Update(Transacao transacao);
+
+        void Remove(Transacao transacao);
     }
 }
--- api/HomeBudget.API/HomeBudget.Infra/Repositories/TransacaoRepository.cs
+++ api/HomeBudget.API/HomeBudget.Infra/Repositories/TransacaoRepository.cs
@@ -1,8 +1,123 @@
-﻿using HomeBudget.Domain.Interfaces;
+﻿using Azure.Core;
+using HomeBudget.Application.DTOs.Pagination;
+using HomeBudget.Application.DTOs.TransacaoDtos;
+using HomeBudget.Domain.Entities;
+using HomeBudget.Domain.Enums;
+using HomeBudget.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace HomeBudget.Infra.Repositories
 {
-    public class TransacaoRepository : ITransacaoRepository
+    public class TransacaoRepository : ITransacaoRepository<PagedRequest>
     {
+        private readonly AppDbContext _context;
+
+        public TransacaoRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Transacao transacao)
+        {
+            await _context.Transacao.AddAsync(transacao);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Transacao?> GetByIdAsync(Guid id)
+        {
+            return await _context.Transacao
+                .AsNoTracking()
+                .FirstOrDefaultAsync(t => t.Id == id);
+        }
+
+        public async Task<(IReadOnlyList<Transacao> Items, int Total)> GetPagedAsync(PagedRequest request)
+        {
+            var safePage = request.Page <= 0 ? 1 : request.Page;
+            var safeSize = request.PageSize <= 0 ? 20 : Math.Min(request.PageSize, 200);
+
+            IQueryable<Transacao> query =
+                _context.Transacao
+                .Include(s => s.Pessoa)
+                .Include(s => s.Categoria)
+                .AsNoTracking();
+
+            query = ApplySearchFilter(query, request.Search);
+            query = ApplyTipoTransacaoFilter(query, (TipoTransacao)request.Tipo);
+            query = ApplySorting(query, request.SortBy, request.SortDir == "asc");
+
+            var total = await query.CountAsync();
+
+            var items = await query
+                .Skip((safePage - 1) * safeSize)
+                .Take(safeSize)
+                .ToListAsync();
+
+            return (items, total);
+        }
+
+        private static IQueryable<Transacao> ApplySearchFilter(
+           IQueryable<Transacao> query,
+           string? search)
+        {
+            if (string.IsNullOrWhiteSpace(search))
+                return query;
+
+            var s = search.Trim();
+            var like = $"%{s}%";
+
+            return query.Where(d =>
+                (d.Pessoa != null && EF.Functions.Like(d.Pessoa.Nome, like)) ||
+                (d.Categoria != null && EF.Functions.Like(d.Categoria.Nome, like)) ||
+                (d.Descricao != null && EF.Functions.Like(d.Descricao, like)));
+        }
+
+
+        private static IQueryable<Transacao> ApplySorting(
+          IQueryable<Transacao> query,
+          string? sortBy,
+          bool asc)
+        {
+            var sort = sortBy?.Trim().ToLowerInvariant();
+
+            return sort switch
+            {
+                "dataCriacao" => asc
+                    ? query.OrderBy(d => d.DataCriacao).ThenBy(d => d.Id)
+                    : query.OrderByDescending(d => d.DataCriacao).ThenBy(d => d.Id),
+
+                "descricao" => asc
+                    ? query.OrderBy(d => d.Descricao).ThenBy(d => d.Id)
+                    : query.OrderByDescending(d => d.DataCriacao).ThenBy(d => d.Id),
+
+                null or "" => query.OrderBy(d => d.DataCriacao),
+                _ => asc
+                    ? query.OrderBy(d => d.DataCriacao).ThenBy(d => d.Id)
+                    : query.OrderByDescending(d => d.DataCriacao).ThenBy(d => d.Id)
+            };
+        }
+
+        private static IQueryable<Transacao> ApplyTipoTransacaoFilter(
+            IQueryable<Transacao> query,
+            TipoTransacao? tipoTransacao)
+        {
+            bool todosTipos = tipoTransacao.HasValue && tipoTransacao.Value == (TipoTransacao)Tipo.Todas;
+            return tipoTransacao.HasValue && !todosTipos
+                ? query.Where(e => e.Tipo == tipoTransacao)
+                : query;
+        }
+
+        public void Remove(Transacao transacao)
+        {
+            _context.Transacao.Remove(transacao);
+            _context.SaveChanges();
+        }
+
+        public void Update(Transacao transacao)
+        {
+            _context.Transacao.Update(transacao);
+            _context.SaveChanges();
+        }
+
+
     }
 }
