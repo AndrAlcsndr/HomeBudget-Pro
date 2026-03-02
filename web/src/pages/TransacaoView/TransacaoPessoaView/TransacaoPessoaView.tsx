d96a0a62006899ec948837d2bc54cb867afd4061
AndrAlcsndr <andralcsndr@gmail.com>
2026-03-02 12:33:18 -0300

    implem. base de view  transacao p/ pessoa

---
+++ web/src/pages/TransacaoView/TransacaoPessoaView/TransacaoPessoaView.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import DataListComponent from "../../../components/DataGridComponent/DataGridComponent";
+import SkeletonComponent from "../../../components/SkeletonComponent/SkeletonComponent";
+
+function TransacaoPessoaView() {
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <div className="container mt-4">
+        {loading ? (
+          <SkeletonComponent show={true} as="list" times={3} />
+        ) : (
+          <DataListComponent columns={columns} rows={rows} />
+        )}
+    </div>
+  );
+}
+
+export default TransacaoPessoaView;
