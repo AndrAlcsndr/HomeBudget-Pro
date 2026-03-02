86eee2eae673548bc77a2494bfde1f5ca3c8ab34
AndrAlcsndr <andralcsndr@gmail.com>
2026-03-02 00:18:02 -0300

    implm. de props

--- web/src/components/ModalSuccessErrorComponent/ModalSuccessErrorComponent.tsx
+++ web/src/components/ModalSuccessErrorComponent/ModalSuccessErrorComponent.tsx
@@ -1,3 +1,13 @@
+import { faCheckCircle, faTimesCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Modal, Button } from "react-bootstrap";
+
+interface Props {
+  show: boolean;
+  type: "success" | "error";
+  message: string;
+  onClose: () => void;
+}
 
 function ModalSuccessError({ show, type, message, onClose }: Props) {
   const isSuccess = type === "success";
