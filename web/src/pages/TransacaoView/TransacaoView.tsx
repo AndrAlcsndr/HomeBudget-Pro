fb59082ad3331fa39315dd3e6cb1805423b9c405
AndrAlcsndr <andralcsndr@gmail.com>
2026-03-02 12:32:19 -0300

    implem. inicial de view de transação com abas

--- web/src/pages/TransacaoView/TransacaoView.tsx
+++ web/src/pages/TransacaoView/TransacaoView.tsx
@@ -1,8 +1,35 @@
+import { useState } from "react";
+import { Tab, Tabs } from "react-bootstrap";
+import ModalCriarEditarTransacoes from "../../components/ModalCriarEditarTransacoesComponent/ModalCriarEditarTransacao";
+import ModalConfirmarCancelar from "../../components/ModalConfirmarCancelarComponent/ModalConfirmarCancelarComponent";
+import ModalSuccessError from "../../components/ModalSuccessErrorComponent/ModalSuccessErrorComponent";
+
 function TransacaoView() {
+  const [key, setKey] = useState("pessoa");
+
   return (
-    <div className="flex">
-      TransacaoView
-    </div>
+    <>
+      <Tabs
+      id="controlled-tab-example"
+      activeKey={key}
+      onSelect={(k) => setKey(k!)}
+      className="mb-3"
+    >
+      <Tab eventKey="pessoa" title="Gastos p/ pessoa">
+        <TransacaoPessoaView />
+      </Tab>
+      <Tab eventKey="categoria" title="Gastos p/ categoria">
+        <TransacaoCategoriaView />
+      </Tab>
+      <Tab eventKey="geral" title="Gastos gerais">
+         <TransacaoGeralView />
+      </Tab>
+    </Tabs>
+
+    <ModalCriarEditarTransacoes />
+    <ModalConfirmarCancelar />
+    <ModalSuccessError />
+    </>
   );
 }
 
